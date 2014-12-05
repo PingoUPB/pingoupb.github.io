99f4186d2c875c0f10a41b4df9e7afa64c3d6158
Christoph Bach <christoph-bach@gmx.de>
2014-12-05 17:57:50 +0100

    new landing header

--- assets/js/scripts.coffee
+++ assets/js/scripts.coffee
@@ -12,3 +12,17 @@ for item in onlyInGerman
   item.style.display = "none"  unless userIsGerman
 for item in notInGerman
   item.style.display = "none"  if userIsGerman
+
+
+swapMenu = () ->
+	offset = window.pageYOffset | document.body.scrollTop;
+	menu = document.querySelector(".main-nav")
+	console.log menu
+
+	if offset > 80
+		menu.classList.remove("main-nav--header")
+	else 
+		menu.classList.add("main-nav--header")
+
+window.onscroll = swapMenu
+
--- gulpfile.js
+++ gulpfile.js
@@ -40,16 +40,18 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
-    return gulp.src('assets/scss/main.scss')
-        .pipe(sass({
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 8 versions', '> 1%', 'android 2.3', 'ie 8'], { cascade: true }))
-        .pipe(minify())
-        .pipe(gulp.dest('assets/css/'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('_site/assets/css/'));
+gulp.task('sass', function() {
+  return gulp.src('assets/scss/*.scss')
+    .pipe(sass({ 
+        style: 'expanded', 
+        onError: browserSync.notify 
+    }))
+    .pipe(prefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(minify())
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(gulp.dest('_site/assets/css/'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 /**
@@ -57,7 +59,7 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/scss/*.scss', ['sass']);
+    gulp.watch('assets/scss/**/*.scss', ['sass']);
     gulp.watch(['index.html', '_layouts/*.html', '_includes/**/*.html', '_data/*.yml', '_posts/*'], ['jekyll-rebuild']);
 });
 
